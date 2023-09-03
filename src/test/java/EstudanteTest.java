24e0e88d2aa525b96d9cc08a68c86e4446a59483
Antonio Dias <addsjunior@gmail.com>
2023-09-02 23:48:03 -0300

    Novos exercícios

--- src/test/java/EstudanteTest.java
+++ src/test/java/EstudanteTest.java
@@ -3,38 +3,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class EstudanteTest {
 
     @org.junit.jupiter.api.Test
-    void getNome() {
+    void testNome() {
         Estudante e = new Estudante("Antonio", 12345);
 
+        // O nome deve ser igual ao atribuído na instanciação
         String nome = e.getNome();
-
         assertEquals("Antonio", nome);
-    }
 
-    @org.junit.jupiter.api.Test
-    void setNome() {
-        Estudante e = new Estudante("Antonio", 12345);
-
+        // O nome pode ser redefinido
         e.setNome("Maria");
-
         assertEquals("Maria", e.getNome());
     }
 
     @org.junit.jupiter.api.Test
-    void getMatricula() {
+    void testMatricula() {
         Estudante e = new Estudante("Antonio", 12345);
 
+        // A matrícula deve ser igual à atribuído na instanciação
         int matricula = e.getMatricula();
-
         assertEquals(12345, matricula);
-    }
 
-    @org.junit.jupiter.api.Test
-    void setMatricula() {
-        Estudante e = new Estudante("Antonio", 12345);
-
+        // A matrícula pode ser redefinida
         e.setMatricula(12346);
-
         assertEquals(12346, e.getMatricula());
     }
 }
